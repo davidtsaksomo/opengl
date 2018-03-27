5544a9c7c09cf1ddbfc7d18e2627c2decec98efa
rozium <fahrurrozi31@gmail.com>
2018-03-27 22:25:19 +0700

    menyelesaikan masalah tanpa masalah

--- Car_VAO/Cars/Car_VAO/main.cpp
+++ Car_VAO/Cars/Car_VAO/main.cpp
@@ -18,10 +18,10 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 using namespace std;
-
+#define width 1300
+#define height 700
 #pragma comment(lib,"glew32.lib")
 
-GLuint ID;
 
 class Point {
 public:
@@ -98,6 +98,25 @@ Vertices rodabelakang;
 Vertices rodakecilbelakang;
 Vertices velgbelakang;
 
+GLuint garis1ID;
+GLuint garis2ID;
+GLuint garismobilID;
+GLuint gedungID;
+GLuint jalanID;
+GLuint kacabelakangID;
+GLuint kacadepanID;
+GLuint lampubelakangID;
+GLuint lampudepan1ID;
+GLuint lampudepan2ID;
+GLuint sasismobilID;
+GLuint spionID;
+GLuint rodadepanID;
+GLuint rodakecildepanID;
+GLuint velgdepanID;
+GLuint rodabelakangID;
+GLuint rodakecilbelakangID;
+GLuint velgbelakangID;
+
 void init() {
 
 	//read from file
@@ -125,16 +144,36 @@ void init() {
 	glShadeModel(GL_FLAT);
 	glEnableClientState(GL_VERTEX_ARRAY);
 
-	float spionData[9][2];
+	
+	
+	float garis1Data[50][2];
+	float garis2Data[50][2];
+	float garismobilData[50][2];
+	float gedungData[50][2];
+	float jalanData[50][2];
+	float kacabelakangData[50][2];
+	float kacadepanData[50][2];
+	float lampubelakangData[50][2];
+	float lampudepan1Data[50][2];
+	float lampudepan2Data[50][2];
+	float sasismobilData[50][2];
+	float spionData[50][2];
+	float rodadepanData[50][2];
+	float rodakecildepanData[50][2];
+	float velgdepanData[50][2];
+	float rodabelakangData[50][2];
+	float rodakecilbelakangData[50][2];
+	float velgbelakangData[50][2];
+
 	for (int i = 0; i < spion.neff; i++) {
 		spionData[i][0] = spion.vertices[i].x;
-		spionData[i][1] = spion.vertices[i].y;
-		cout << spionData[i][0] << " , " << spionData[i][1] << endl;
+		spionData[i][1] = height - spion.vertices[i].y;
 	}
-	
-	glGenBuffers(1, &ID);
-	glBindBuffer(GL_ARRAY_BUFFER, ID);
-	glBufferData(GL_ARRAY_BUFFER, spion.neff, spionData, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &spionID);
+	glBindBuffer(GL_ARRAY_BUFFER, spionID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(spionData) , spionData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void reshape(int w, int h) {
@@ -144,19 +183,36 @@ void reshape(int w, int h) {
 	gluOrtho2D(0.0f, (GLdouble)w, 0.0f, (GLdouble)h);
 }
 
+static GLfloat ANGLE = 0;
+static GLfloat XTRANSLATED_JALAN = 0.5;
+static GLfloat XTRANSLATED_GEDUNG = 2.3;
+static GLfloat RED_LANGIT = 135.0;
+static GLfloat GREEN_LANGIT = 206.0;
+static GLfloat BLUE_LANGIT = 250.0;
+static GLboolean kePagi = false;
+
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glBindBuffer(GL_ARRAY_BUFFER, ID);
+	glClearColor(RED_LANGIT / 255.0, GREEN_LANGIT / 255.0, BLUE_LANGIT / 255.0, 1);  // sky color
+	glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	
+	glColor3f(237.0 / 255.0, 2.0 / 255.0, 2.0 / 255.0);
+	glBindBuffer(GL_ARRAY_BUFFER, spionID);
 	glVertexPointer(2, GL_FLOAT, 2 * sizeof(float), 0);
-	glDrawArrays(GL_LINE_LOOP, 0, spion.neff);
+	glDrawArrays(GL_POLYGON, 0, spion.neff);
+
+	//glColor3f(113.0 / 255.0, 113.0 / 255.0, 115.0 / 255.0);
+	//glBindBuffer(GL_ARRAY_BUFFER, jalanID);
+	//glVertexPointer(2, GL_FLOAT, 2 * sizeof(float), 0);
+	//glDrawArrays(GL_POLYGON, 0, jalan.neff);
+
+
 	glFlush();
 }
 
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(1366, 768);
+	glutInitWindowSize(width, height);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("GL Cars");
 	init();
