c20c5f204ae82254388e0b5154a82d1a7f2a5f70
rozium <fahrurrozi31@gmail.com>
2018-05-16 13:15:52 +0700

    fix bug

---
+++ 3DCarParticle/3DCarParticle/Program.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+using WMPLib;
+
+namespace CarParticle
+{
+    class Program
+    {
+        
+        private static CarModel carmodel;
+        private static Smoke smoke;
+        private static Rain rain;
+
+        private static int width = 1280, height = 720;
+        private static ShaderProgram program;
+
+        private static System.Diagnostics.Stopwatch watch;
+        private static float xangle, yangle;
+        private static bool autoRotate, lighting = true, fullscreen = false, alpha = true;
+        private static bool left, right, up, down;
+
+        private static float maximumAmbient = 10f;
+        private static float minimumAmbient = 0f;
+        private static float maximumDiffuse = 10f;
+        private static float minimumDiffuse = 0f;
+        //TODO: bikin slider buat modifikasi nilai-nilai ini
+        private static float ambient = 0.3f;
+        private static float maxdiffuse = 1f;
+        private static float time = 0f;
+        private static int timedir = 1;
+        private static WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+
+        static void Main(string[] args)
+        {
+            wplayer.URL = "song.mp3";
+            wplayer.controls.stop();
+            
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH | Glut.GLUT_MULTISAMPLE);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("UAS IF3260 - Grafika Komputer");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutKeyboardFunc(OnKeyboardDown);
+            Glut.glutKeyboardUpFunc(OnKeyboardUp);
+            Glut.glutMouseWheelFunc(MouseWheel);
+
+            Glut.glutCloseFunc(OnClose);
+            Glut.glutReshapeFunc(OnReshape);
+
+            Gl.Enable(EnableCap.DepthTest);
+            Gl.Enable(EnableCap.Multisample);
+            Gl.Enable(EnableCap.ProgramPointSize);
+            Gl.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.One);
+
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 6), new Vector3(1.2f,0.2f,0f), new Vector3(0, 1, 0)));
+
+            program["light_direction"].SetValue(new Vector3(0, 0, 1));
+            program["enable_lighting"].SetValue(lighting);
+
+            carmodel = new CarModel();
+            rain = new Rain();
+            smoke = new Smoke();
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            Glut.glutMainLoop();
+            
+
+        }
+
+        private static void OnClose()
+        {
+            CarModel.cube.Dispose();
+            CarModel.cubeNormals.Dispose();
+            CarModel.cubeUV.Dispose();
+            CarModel.cubeQuads.Dispose();
+            CarModel.bodyTexture.Dispose();
+            CarModel.kacaTexture.Dispose();
+            CarModel.rodaTexture.Dispose();
+
+            Rain.OnClose();
+            Smoke.OnClose();
+            program.DisposeChildren = true;
+            program.Dispose();
+
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            watch.Stop();
+            float deltaTime = (float)watch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency;
+            watch.Restart();
+            Console.WriteLine("FPS : " + 1f / deltaTime);
+            time += deltaTime * timedir;
+            // perform rotation of the cube depending on the keyboard state
+            if (autoRotate)
+            {
+                xangle += deltaTime / 2;
+                yangle += deltaTime;
+            }
+            if (right) yangle += deltaTime;
+            if (left) yangle -= deltaTime;
+            if (up) xangle -= deltaTime;
+            if (down) xangle += deltaTime;
+
+            // set up the viewport and clear the previous depth and color buffers
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            //Gl.ClearColor(255f/255,255f/255,255f/255,255f/255);
+            // make sure the shader program and texture are being used
+            Gl.UseProgram(program);
+            Gl.BindTexture(CarModel.bodyTexture);
+            Gl.Disable(EnableCap.Blend);
+            Gl.Enable(EnableCap.DepthTest);
+
+            // set up the model matrix and draw the cube
+            program["model_matrix"].SetValue(Matrix4.CreateRotationY(yangle)  * Matrix4.CreateRotationX(xangle));
+            program["enable_lighting"].SetValue(lighting);
+
+            Gl.BindBufferToShaderAttribute(CarModel.cube, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(CarModel.cubeNormals, program, "vertexNormal");
+            Gl.BindBufferToShaderAttribute(CarModel.cubeUV, program, "vertexUV");
+            Gl.BindBuffer(CarModel.cubeQuads);
+            
+            Gl.DrawElements(BeginMode.Quads, CarModel.cubeQuads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+
+            //Draw window
+            Gl.BindTexture(CarModel.kacaTexture);
+            
+            program["max_diffuse"].SetValue(maxdiffuse);
+            program["ambient"].SetValue(ambient);
+
+            Gl.BindBufferToShaderAttribute(CarModel.window, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(CarModel.windowNormals, program, "vertexNormal");
+            Gl.BindBufferToShaderAttribute(CarModel.windowUV, program, "vertexUV");
+            Gl.BindBuffer(CarModel.windowQuads);
+
+            Gl.DrawElements(BeginMode.Quads, CarModel.windowQuads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            //drawwheel
+            Gl.BindTexture(CarModel.rodaTexture);
+
+            Gl.BindBufferToShaderAttribute(CarModel.wheel1, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(CarModel.wheel1Normals, program, "vertexNormal");
+            Gl.BindBufferToShaderAttribute(CarModel.wheel1UV, program, "vertexUV");
+            Gl.BindBuffer(CarModel.wheel1Quads);
+            Gl.DrawElements(BeginMode.TriangleStrip, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.BindBufferToShaderAttribute(CarModel.wheel2, program, "vertexPosition");
+            Gl.DrawElements(BeginMode.TriangleStrip, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.BindBufferToShaderAttribute(CarModel.wheel2Normals, program, "vertexNormal");
+
+            Gl.BindBufferToShaderAttribute(CarModel.wheel3, program, "vertexPosition");
+            Gl.DrawElements(BeginMode.TriangleStrip, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.BindBufferToShaderAttribute(CarModel.wheel4, program, "vertexPosition");
+            Gl.DrawElements(BeginMode.TriangleStrip, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            //Gl.BindTexture(velgTexture);
+            Gl.BindBufferToShaderAttribute(CarModel.wheel1Normals, program, "vertexNormal");
+
+            Gl.BindBufferToShaderAttribute(CarModel.velg1, program, "vertexPosition");
+            Gl.DrawElements(BeginMode.TriangleFan, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.BindBufferToShaderAttribute(CarModel.velg2, program, "vertexPosition");
+
+            Gl.DrawElements(BeginMode.TriangleFan, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.BindBufferToShaderAttribute(CarModel.wheel2Normals, program, "vertexNormal");
+
+
+            Gl.BindBufferToShaderAttribute(CarModel.velg3, program, "vertexPosition");
+            Gl.DrawElements(BeginMode.TriangleFan, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.BindBufferToShaderAttribute(CarModel.velg4, program, "vertexPosition");
+            Gl.DrawElements(BeginMode.TriangleFan, CarModel.wheel1Quads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.Enable(EnableCap.Blend);
+            Gl.Disable(EnableCap.DepthTest);
+
+            //Draw Rain
+            // make sure the shader program and texture are being used
+            Gl.UseProgram(Rain.program.ProgramID);
+            Gl.BindTexture(Rain.particleTexture);
+            Rain.program["model_matrix"].SetValue(Matrix4.CreateRotationY(yangle) * Matrix4.CreateRotationX(xangle));
+
+            // update our particle list
+            for (int i = 0; i < Rain.particles.Count; i++)
+            {
+                Rain.particles[i].Update(deltaTime);
+                //if (particles[i].Life < 0) particles[i] = new Particle(Vector3.Zero);
+                if (Rain.particles[i].Life < 0) Rain.particles[i] = new Rain.Particle(new Vector3((float)Rain.generator.NextDouble() * 50 - 25, 20, (float)Rain.generator.NextDouble() * 50 - 25));
+                Rain.particlePositions[i] = Rain.particles[i].Position;
+            }
+
+            // delete our previous particle positions (if applicable) and then create a new VBO
+            if (Rain.particleVertices != null) Rain.particleVertices.Dispose();
+            Rain.particleVertices = new VBO<Vector3>(Rain.particlePositions);
+
+            // bind the VBOs to their shader attributes
+            Gl.BindBufferToShaderAttribute(Rain.particleVertices, Rain.program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(Rain.particleColors, Rain.program, "vertexColor");
+            Gl.BindBuffer(Rain.particlePoints);
+
+            // enable point sprite mode (which enables the gl_PointCoord value)
+            Gl.Enable(EnableCap.PointSprite);
+            Gl.DrawElements(BeginMode.Points, Rain.particlePoints.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.Disable(EnableCap.PointSprite);
+
+
+            //Draw Smoke
+            // make sure the shader program and texture are being used
+            Gl.UseProgram(Smoke.program.ProgramID);
+            Gl.BindTexture(Smoke.particleTexture);
+            Smoke.program["model_matrix"].SetValue(Matrix4.CreateRotationY(yangle) * Matrix4.CreateRotationX(xangle));
+            if (time > 5f)
+            {
+                timedir = -1;
+            }
+            else if(time < 0f)
+            {
+                timedir = 1;
+
+            }
+            Smoke.program["mixer"].SetValue(time/2.5f);
+
+            // update our particle list
+            for (int i = 0; i < Smoke.particles.Count; i++)
+            {
+                Smoke.particles[i].Update(deltaTime);
+                //if (particles[i].Life < 0) particles[i] = new Particle(Vector3.Zero);
+                if (Smoke.particles[i].Life < 0) Smoke.particles[i] = new Smoke.Particle(new Vector3(1.8f, 0.2f, 0.3f));
+                Smoke.particlePositions[i] = Smoke.particles[i].Position;
+            }
+
+            // delete our previous particle positions (if applicable) and then create a new VBO
+            if (Smoke.particleVertices != null) Smoke.particleVertices.Dispose();
+            Smoke.particleVertices = new VBO<Vector3>(Smoke.particlePositions);
+
+            // bind the VBOs to their shader attributes
+            Gl.BindBufferToShaderAttribute(Smoke.particleVertices, Smoke.program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(Smoke.particleColors, Smoke.program, "vertexColor");
+            Gl.BindBuffer(Smoke.particlePoints);
+
+            // enable point sprite mode (which enables the gl_PointCoord value)
+            Gl.Enable(EnableCap.PointSprite);
+            Gl.DrawElements(BeginMode.Points, Smoke.particlePoints.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Gl.Disable(EnableCap.PointSprite);
+
+
+            Glut.glutSwapBuffers();
+
+        }
+
+        private static void OnReshape(int width, int height)
+        {
+            Program.width = width;
+            Program.height = height;
+
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            Gl.UseProgram(Rain.program.ProgramID);
+            Rain.program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+        }
+
+        private static void OnKeyboardDown(byte key, int x, int y)
+        {
+            if (key == 'w') up = true;
+            else if (key == 's') down = true;
+            else if (key == 'd') right = true;
+            else if (key == 'a') left = true;
+            else if (key == 'p')
+            {
+                if (maxdiffuse <= maximumDiffuse)
+                {
+                    maxdiffuse = maxdiffuse + 0.1f;
+                }
+            }
+            else if (key == 'o')
+            {
+                if (maxdiffuse >= minimumDiffuse)
+                {
+                    maxdiffuse = maxdiffuse - 0.1f;
+                }
+            }
+            else if (key == ']')
+            {
+                if (ambient <= maximumAmbient)
+                {
+                    ambient = ambient + 0.05f;
+                }
+            }
+            else if (key == '[')
+            {
+                if (ambient >= minimumAmbient)
+                {
+                    ambient = ambient - 0.05f;
+                }
+            }
+            else if (key == 27) Glut.glutLeaveMainLoop();
+
+            if (key == 27) Glut.glutLeaveMainLoop();
+
+        }
+
+        private static void OnKeyboardUp(byte key, int x, int y)
+        {
+            if (key == 'w') up = false;
+            else if (key == 's') down = false;
+            else if (key == 'd') right = false;
+            else if (key == 'a') left = false;
+            else if (key == ' ')
+            {
+                autoRotate = !autoRotate;
+                if (autoRotate) {
+                    wplayer.controls.play();
+
+                }
+                else
+                    wplayer.controls.stop();
+
+                Rain.rainbow = !Rain.rainbow;
+                Rain.program.Use();
+                Rain.program["static_colors"].SetValue(Rain.rainbow);
+
+                Smoke.rainbow = !Smoke.rainbow;
+                Smoke.program.Use();
+                Smoke.program["static_colors"].SetValue(Smoke.rainbow);
+
+            }
+            else if (key == 'l') lighting = !lighting;
+            else if (key == 'z')
+            {
+                fullscreen = !fullscreen;
+                if (fullscreen) Glut.glutFullScreen();
+                else
+                {
+                    Glut.glutPositionWindow(0, 0);
+                    Glut.glutReshapeWindow(1280, 720);
+                }
+            }
+            else if (key == 'b')
+            {
+                alpha = !alpha;
+                if (alpha)
+                {
+                    Gl.Enable(EnableCap.Blend);
+                    Gl.Disable(EnableCap.DepthTest);
+                }
+                else
+                {
+                    Gl.Disable(EnableCap.Blend);
+                    Gl.Enable(EnableCap.DepthTest);
+                }
+            }
+            if (key == 'f')
+            {
+                fullscreen = !fullscreen;
+                if (fullscreen) Glut.glutFullScreen();
+                else
+                {
+                    Glut.glutPositionWindow(0, 0);
+                    Glut.glutReshapeWindow(1280, 720);
+                }
+            }
+            else if (key == 'r')
+            {
+                Rain.rainbow = !Rain.rainbow;
+                Rain.program.Use();
+                Rain.program["static_colors"].SetValue(Rain.rainbow);
+
+                Smoke.rainbow = !Smoke.rainbow;
+                Smoke.program.Use();
+                Smoke.program["static_colors"].SetValue(Smoke.rainbow);
+            }
+        }
+
+        private static void MouseWheel(int wheel, int direction, int x, int y)
+        {
+            if (direction > 0)
+            {
+                if (maxdiffuse <= maximumDiffuse)
+                {
+                    maxdiffuse = maxdiffuse + 0.1f;
+                }
+            } else
+            {
+                if (maxdiffuse >= minimumDiffuse)
+                {
+                    maxdiffuse = maxdiffuse - 0.1f;
+                }
+            }
+        }
+
+        public static string VertexShader = @"
+#version 130
+
+in vec3 vertexPosition;
+in vec3 vertexNormal;
+in vec2 vertexUV;
+
+out vec3 normal;
+out vec2 uv;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void)
+{
+    normal = normalize((model_matrix * vec4(floor(vertexNormal), 0)).xyz);
+    uv = vertexUV;
+
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        public static string FragmentShader = @"
+#version 130
+
+uniform sampler2D texture;
+uniform vec3 light_direction;
+uniform bool enable_lighting;
+uniform float ambient;
+uniform float max_diffuse;
+
+in vec3 normal;
+in vec2 uv;
+
+out vec4 fragment;
+
+void main(void)
+{
+    float diffuse = max(dot(normal, light_direction), 0) * max_diffuse;
+    float lighting = (enable_lighting ? max(diffuse, ambient) : 1);
+
+    // add in some blending for tutorial 8 by setting the alpha to 0.5
+    fragment = vec4(lighting * texture2D(texture, uv).xyz, 1);
+}
+";
+    }
+}
---
+++ 3DCarParticle/3DCarParticle/Rain.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace CarParticle
+{
+    public class Rain
+    {
+        public static int width = 1280, height = 720;
+        public static bool fullscreen = false, rainbow = false;
+        public static System.Diagnostics.Stopwatch watch;
+
+        public static ShaderProgram program;
+        public static Texture particleTexture;
+        public static VBO<Vector3> particleVertices;
+        public static VBO<Vector3> particleColors;
+        public static VBO<int> particlePoints;
+
+        public static List<Particle> particles = new List<Particle>();
+        public static int particleCount = 2000;
+        public static Vector3[] particlePositions = new Vector3[particleCount];
+        public static Random generator = new Random();
+
+
+
+        public Rain()
+        {
+            // create our shader program
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            // set up the projection and view matrix
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 20), Vector3.Zero, new Vector3(0, 1, 0)));
+            program["model_matrix"].SetValue(Matrix4.Identity);
+            program["static_colors"].SetValue(true);
+
+            // load the particle texture
+            particleTexture = new Texture("star.bmp");
+
+            // set up the particlePoints VBO, which will stay constant
+            int[] points = new int[particleCount];
+            for (int i = 0; i < points.Length; i++) points[i] = i;
+            particlePoints = new VBO<int>(points, BufferTarget.ElementArrayBuffer);
+
+            // set up the particleColors, which we'll just keep static
+            Vector3[] colors = new Vector3[particleCount];
+            //for (int i = 0; i < colors.Length; i++) colors[i] = new Vector3((float)generator.NextDouble(), (float)generator.NextDouble(), (float)generator.NextDouble());
+            for (int i = 0; i < colors.Length; i++) colors[i] = new Vector3(1f,1f, 1f);
+            particleColors = new VBO<Vector3>(colors);
+
+            // build up our first batch of 1000 particles and 1000 static colors
+            for (int i = 0; i < particleCount; i++) particles.Add(new Particle(new Vector3((float)generator.NextDouble() * 50 - 25, 20, (float)generator.NextDouble() * 50 - 25), 0));
+
+
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+        }
+
+        public static void OnClose()
+        {
+            particleVertices.Dispose();
+            particleColors.Dispose();
+            particlePoints.Dispose();
+            program.DisposeChildren = true;
+            program.Dispose();
+        }
+
+        public class Particle
+        {
+            public float Life;
+            public Vector3 Position;
+            public Vector3 Direction;
+
+            public Particle(Vector3 origin, float life = 2)
+            {
+                Position = origin;
+                //Direction = new Vector3((float)generator.NextDouble() * 2 - 1, 3 + (float)generator.NextDouble(), (float)generator.NextDouble() * 2 - 1);
+                Direction = new Vector3(0, (float)generator.NextDouble() * -1,0);
+                Life = life + (float)generator.NextDouble() * 2;
+            }
+
+            public void Update(float delta)
+            {
+                Direction += delta * new Vector3(0, -8, 0);
+                Position += Direction * delta;
+                Life -= delta;
+            }
+        }
+
+
+
+        public static string VertexShader = @"
+#version 130
+
+in vec3 vertexPosition;
+in vec3 vertexColor;
+
+out vec3 color;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+uniform bool static_colors;
+
+void main(void)
+{
+    color = (static_colors ? vertexColor : mix(vec3(0, 0, 1), vec3(0.7, 0, 1), clamp(vertexPosition.y / 2, 0, 1)));
+
+    gl_PointSize = 5;
+
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition.xyz, 1);
+}
+";
+
+        public static string FragmentShader = @"
+#version 130
+
+uniform sampler2D texture;
+
+in vec3 color;
+
+out vec4 fragment;
+
+void main(void)
+{
+    fragment = vec4(color * texture2D(texture, gl_PointCoord).xyz, 1);
+}
+";
+    }
+}
